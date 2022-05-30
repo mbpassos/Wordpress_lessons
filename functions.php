420652d90f333d2ebe3b87b67b76022cd88384e3
mbpassos <marilia.bpassos@gmail.com>
2022-05-30 16:08:52 +0100

    adding interior pages and setting links

--- functions.php
+++ functions.php
@@ -2,7 +2,8 @@
 
 function backoffice_files() {    
     wp_enqueue_script('main_script', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
-    // CASO O JAVASCRIPT NÃO TENHA DEPENDÊNCIAS: wp_enqueue_script('main_script', get_theme_file_uri('/build/index.js'), NULL);
+    // O true acima significa que é pra fazer o load do script no final do body para melhor performance
+    // CASO O JAVASCRIPT NÃO TENHA DEPENDÊNCIAS: wp_enqueue_script('main_script', get_theme_file_uri('/build/index.js'), NULL); 
     wp_enqueue_style('main_styles', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('extra_styles', get_theme_file_uri('/build/index.css'));
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
@@ -11,5 +12,11 @@ function backoffice_files() {
 }
 
 add_action('wp_enqueue_scripts', 'backoffice_files'); 
+
+function backoffice_features() {
+    add_theme_support('title-tag');
+}
+
+add_action('after_setup_theme', 'backoffice_features');
 
 
