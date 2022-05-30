9a058ec9bd6747c4b14cac8ffcab5572e3f1be42
mbpassos <marilia.bpassos@gmail.com>
2022-05-30 11:31:32 +0100

    first commit - posts, pages

---
+++ index.php
@@ -0,0 +1,56 @@
+
+<!-- ############## FUNCTIONS LESSON ################################# -->
+
+<!-- <?php
+  function greet($name, $color) {
+    echo "<p>Hi, my name $name is and my favourite color is $color.</p>";
+  }
+
+  greet('John', 'yellow');
+  greet('Jane', 'blue');
+
+?> -->
+<!-- <h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p> -->
+
+
+
+<!-- ############## WHILE LOOPS LESSON ############################## -->
+
+<!-- <?php
+
+  $names = array('Brad', 'John', 'Lila', 'Luna');
+  
+  $count = 0;
+
+  // while($count < 101) {
+  //   echo "<li>$count</li>";
+  //   $count++;
+  // }
+
+  while($count < count($names)) {
+    echo "<li>Hi my name is $names[$count]</li>";
+    $count++;
+  }
+
+?> -->
+
+<!-- <p>Hi, my name is <?php echo $names[2]; ?></p> -->
+
+
+<!-- ############## POSTS LESSON ############################## -->
+
+<?php
+
+while(have_posts()) {
+  the_post(); ?>
+  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+      <?php the_content(); ?>
+      <hr>
+<?php
+}
+
+?>
+
+
+
